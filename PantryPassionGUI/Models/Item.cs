a5f12f53fd907615e8e0d5d1e863684af2c1823c
KongKeano <keanhansen@hotmail.com>
2021-04-14 13:34:53 +0200

    Added FindItem and tempItem

---
+++ PantryPassionGUI/Models/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PantryPassionGUI.Models
+{
+    public class Item
+    {
+        public Item(string name, string ean="", int averageLifespanDays=0, string itemSize="")
+        {
+            Name = name;
+            EAN = ean;
+            AverageLifespanDays = averageLifespanDays;
+            ItemSize = itemSize;
+        }
+
+        public string Name { get; set; }
+        public string EAN { get; set; }
+        public int AverageLifespanDays { get; set; }
+        public string ItemSize { get; set; }
+    }
+}
---
+++ PantryPassionGUI/ViewModels/FindItemViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using PantryPassionGUI.Models;
+
+namespace PantryPassionGUI.ViewModels
+{
+    
+    class FindItemViewModel
+    {
+        public FindItemViewModel()
+        {
+            Item i1 = new Item("Nutella", "42069", 100, "500g");
+            Item i2 = new Item("Kyllingebryst", "1337", 10, "1000g");
+            Item i3 = new Item("Støvsuger", "666", 3, "69kg");
+
+            Items = new ObservableCollection<Item>();
+            Items.Add(i1);
+            Items.Add(i2);
+            Items.Add(i3);
+
+            
+        }
+
+        private FindItem FindItemWindow;
+
+        public ObservableCollection<Item> Items { get; set; }
+
+    }
+}
