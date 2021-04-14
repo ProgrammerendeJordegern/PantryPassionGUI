0cc0261e742cc544525bf87bd0d279d210dd31fa
KongKeano <keanhansen@hotmail.com>
2021-04-14 13:27:32 +0200

    added mainwindow and tempuser and images

---
+++ PantryPassionGUI/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PantryPassionGUI.Models
+{
+    public class tempUser
+    {
+        public tempUser(string name)
+        {
+            Name = name;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
---
+++ PantryPassionGUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using PantryPassionGUI.Models;
+using Prism.Commands;
+
+
+namespace PantryPassionGUI.ViewModels
+{
+    class MainWindowViewModel
+    {
+        public tempUser t1
+        {
+            get;
+            set;
+        }
+        
+        public MainWindowViewModel()
+        {
+            t1 = new tempUser("Jesper");
+        }
+
+        ICommand _addItemCommand;
+
+        public ICommand AddItemCommand
+        {
+            get { return _addItemCommand ?? (_addItemCommand = new DelegateCommand(AddItemExecute)); }
+        }
+
+        void AddItemExecute()
+        {
+            MessageBox.Show("This is where u add an item...");
+        }
+
+        //FindItem
+    }
+}
