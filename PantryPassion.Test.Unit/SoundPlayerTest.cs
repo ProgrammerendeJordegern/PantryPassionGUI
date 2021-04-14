5ce55c4e82fe2bdf4f9b0b429c752772d07784c0
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2021-04-14 13:52:00 +0200

    test for soundplayer

---
+++ PantryPassion.Test.Unit/SoundPlayerTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using PantryPassionGUI.Models;
+
+namespace PantryPassion.Test.Unit
+{
+    class SoundPlayerTest
+    {
+        private SoundPlayer _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new SoundPlayer();
+        }
+
+        [Test]
+        public void Mute_SetDefult_False()
+        {
+            Assert.That(_uut.Mute, Is.False);
+        }
+
+        [Test]
+        public void Mute_Set_True()
+        {
+            _uut.Mute = true;
+            Assert.That(_uut.Mute,Is.True);
+        }
+    }
+}
