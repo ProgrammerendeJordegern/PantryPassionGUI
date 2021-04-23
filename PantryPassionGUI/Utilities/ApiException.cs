768c2741077c042958141124e1e9f0f9827635c8
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2021-04-23 12:00:45 +0200

    startet

---
+++ PantryPassionGUI/Utilities/ApiException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ConsoleAppClient.Utilities
+{
+    public class ApiException : Exception
+    {
+        public int StatusCode { get; set; }
+
+        public string Content { get; set; }
+    }
+}
