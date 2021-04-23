e3e6a6b06e582e7ca621713d80676284fd0b0e32
KongKeano <keanhansen@hotmail.com>
2021-04-23 10:54:06 +0200

    Started login logout wpf

---
+++ PantryPassionGUI/Views/LoginWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PantryPassionGUI.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
