637080f68e525d4d6b35ad5a6e31b01f812732f2
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2021-04-25 20:36:06 +0200

    added test

---
+++ PantryPassion.Test.Unit/AddItemViewModelTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PantryPassionGUI.Models;
+using PantryPassionGUI.ViewModels;
+
+namespace PantryPassion.Test.Unit
+{
+    class AddItemViewModelTest
+    {
+        private AddItemViewModel _uut;
+        private object _obj;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new AddItemViewModel();
+            _obj = new object();
+        }
+
+        [Test]
+        public void AddItemViewModel_UpArrowCommand_AddOneToAmount()
+        {
+            _uut.UpArrowCommand.Execute(_obj);
+            Assert.That(_uut.InventoryItem.Amount,Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AddItemViewModel_DownArrowCommand_SubtractOneToAmount()
+        {
+            _uut.UpArrowCommand.Execute(_obj);
+            _uut.UpArrowCommand.Execute(_obj);
+            _uut.UpArrowCommand.Execute(_obj);
+
+            _uut.DownArrowCommand.Execute(_obj);
+            Assert.That(_uut.InventoryItem.Amount, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void AddItemViewModel_DownArrowCommand_CanNotExecute()
+        {
+            Assert.That(_uut.DownArrowCommand.CanExecute(_obj), Is.False);
+        }
+
+        [Test]
+        public void AddItemViewModel_DownArrowCommand_CanExecute()
+        {
+            _uut.UpArrowCommand.Execute(_obj);
+            Assert.That(_uut.DownArrowCommand.CanExecute(_obj), Is.True);
+        }
+    }
+}
