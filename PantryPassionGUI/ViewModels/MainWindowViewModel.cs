d9a7bdd0d3e948cd3ff8907f0fa7cfad186066aa
Simon <simonkjaer449@gmail.com>
2021-04-28 14:41:00 +0200

    Made a window to use FindeItem as a usercontrol

--- PantryPassionGUI/ViewModels/MainWindowViewModel.cs
+++ PantryPassionGUI/ViewModels/MainWindowViewModel.cs
@@ -85,6 +85,7 @@ namespace PantryPassionGUI.ViewModels
         void FindItemExecute()
         {
             FindItemWindow FIWindow = new FindItemWindow();
+            
             //FIWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
             //if (FIWindow.ShowDialog() == true)
             //{
