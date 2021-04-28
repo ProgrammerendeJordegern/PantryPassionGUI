26bca012d09ad679ecc3b1667892aaa1fbf972cf
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2021-04-28 13:50:18 +0200

    Added test to RemoveItemViewModel

--- PantryPassion.Test.Unit/ViewModelTest/RemoveItemViewModelTest.cs
+++ PantryPassion.Test.Unit/ViewModelTest/RemoveItemViewModelTest.cs
@@ -1,4 +1,5 @@
-﻿using NSubstitute;
+﻿using AForge.Video.DirectShow;
+using NSubstitute;
 using NUnit.Framework;
 using PantryPassionGUI.Models;
 using PantryPassionGUI.ViewModels;
@@ -20,5 +21,67 @@ namespace PantryPassion.Test.Unit.ViewModelTest
             _uut = new RemoveItemViewModel(_cameraViewModel, _backendConnection, 5);
             _obj = new object();
         }
+
+        [Test]
+        public void RemoveItemViewModel_UpArrowCommand_AddOneToAmount()
+        {
+            _uut.UpArrowCommand.Execute(_obj);
+            Assert.That(_uut.InventoryItem.Amount, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void RemoveItemViewModel_UpArrowCommand_CanExecute()
+        {
+            Assert.That(_uut.UpArrowCommand.CanExecute(_obj),Is.True);
+        }
+
+        [Test]
+        public void RemoveItemViewModel_UpArrowCommand_CanNotExecute()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                _uut.UpArrowCommand.Execute(_obj);
+            }
+            Assert.That(_uut.UpArrowCommand.CanExecute(_obj), Is.False);
+        }
+
+        [Test]
+        public void RemoveItemViewModel_DownArrowCommand_SubtractOneToAmount()
+        {
+            _uut.UpArrowCommand.Execute(_obj);
+            _uut.UpArrowCommand.Execute(_obj);
+
+            _uut.DownArrowCommand.Execute(_obj);
+            Assert.That(_uut.InventoryItem.Amount, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void RemoveItemViewModel_DownArrowCommand_CanNotExecute()
+        {
+            Assert.That(_uut.DownArrowCommand.CanExecute(_obj), Is.False);
+        }
+
+        [Test]
+        public void RemoveItemViewModel_DownArrowCommand_CanExecute()
+        {
+            _uut.UpArrowCommand.Execute(_obj);
+            Assert.That(_uut.DownArrowCommand.CanExecute(_obj), Is.True);
+        }
+
+        [Test]
+        public void RemoveItemViewModel_OkCommand_CalledCameraOff()
+        {
+            _uut.OkCommand.Execute(_obj);
+            _cameraViewModel.Camera.Received(1).CameraOff();
+        }
+
+        [Test]
+        public void RemoveItemViewModel_CancelCommand_CalledCameraOff()
+        {
+            _uut.CancelCommand.Execute(_obj);
+            _cameraViewModel.Camera.Received(1).CameraOff();
+        }
+
+        //Need test for removeItemCommand
     }
 }
