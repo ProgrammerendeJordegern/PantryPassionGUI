f3afd6cfa928b1cf11f787fd7f36e3f21ed0de9d
Simon <simonkjaer449@gmail.com>
2021-05-06 15:56:04 +0200

    Added close methods to shoppinglist, and removed som commeted code in add, find and remove item

--- PantryPassionGUI/ViewModels/ShoppingListViewModel.cs
+++ PantryPassionGUI/ViewModels/ShoppingListViewModel.cs
@@ -34,6 +34,7 @@ namespace PantryPassionGUI.ViewModels
             ItemsList = new ObservableCollection<InventoryItem>();
             FindItemViewModel = new FindItemViewModel();
             SharedOberserverableCollection = SharedOberserverableCollectionOfInventoryItems.Instance();
+            ItemsList = SharedOberserverableCollection.SharedInventoryItems;
         }
 
         public ICommand AutoGenerateListCommand
