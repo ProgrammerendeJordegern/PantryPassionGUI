24b58b62c0c3d85bb7545ce9ad0f4be2bbecd0d4
Laurits H.G <201905745@post.au.dk>
2021-05-07 11:07:01 +0200

    Added Logout

---
+++ PantryPassionGUI/Operations/ApiOperations.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using PantryPassionGUI.Models;
+using Newtonsoft.Json;
+
+using System.Security.Cryptography;
+
+namespace PantryPassionGUI.Operations
+{
+    // This class and most of its code is taken from https://auth0.com/blog/add-auth-to-native-desktop-csharp-apps-with-jwt/#Let-s-get-started
+    public class ApiOperations
+    {
+        private string baseUrl;
+
+        public ApiOperations()
+        {
+            this.baseUrl = "http://localhost:5000/api";
+        }
+
+        /**
+ * Authenticate user with Web Api Endpoint
+ * @param string username
+ * @param string password
+ */
+        public User AuthenticateUser(string username, string password)
+        {
+            string endpoint = this.baseUrl + "/users/login";
+            string method = "POST";
+            string json = JsonConvert.SerializeObject(new
+            {
+                username = username,
+                //password = HashUser(username,password)
+                //password = BCrypt.Net.BCrypt.HashPassword(username+password,11)
+                password = password
+            });
+
+            WebClient wc = new WebClient();
+            wc.Headers["Content-Type"] = "application/json";
+            try
+            {
+                string response = wc.UploadString(endpoint, method, json);
+                return JsonConvert.DeserializeObject<User>(response);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        /**
+ * Get User Details from Web Api
+ * @param  User Model
+ */
+        public User GetUserDetails(User user)
+        {
+            string endpoint = this.baseUrl + "/users/" + user.Id;
+            string access_token = user.access_token;
+
+            WebClient wc = new WebClient();
+            wc.Headers["Content-Type"] = "application/json";
+            wc.Headers["Authorization"] = access_token;
+            try
+            {
+                string response = wc.DownloadString(endpoint);
+                user = JsonConvert.DeserializeObject<User>(response);
+                user.access_token = access_token;
+                return user;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        /**
+ * Register User
+ * @param  string username
+ * @param  string password
+ * @param  string firstname
+ * @param  string lastname
+ * @param  string middlename
+ * @param  int    age
+ */
+        public User RegisterUser(string username, string password, string fullname)
+        {
+            string endpoint = this.baseUrl + "/users";
+            string method = "POST";
+            string json = JsonConvert.SerializeObject(new
+            {
+                username = username,
+                //password = HashUser(username,password),
+                //password = BCrypt.Net.BCrypt.HashPassword(username + password, 11),
+                password = password,
+                fullname = fullname
+            });
+
+            WebClient wc = new WebClient();
+            wc.Headers["Content-Type"] = "application/json";
+            try
+            {
+                string response = wc.UploadString(endpoint, method, json);
+                return JsonConvert.DeserializeObject<User>(response);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public void Logout()
+        {
+            Globals.LoggedInUser.access_token = null;
+            Globals.LoggedInUser = null;
+        }
+        public byte[] HashUser(string username, string password)
+        {
+            string tmpPassword;
+
+            tmpPassword = username.ToLower() + password;
+
+
+            UTF8Encoding textConverter = new UTF8Encoding();
+            byte[] passBytes = textConverter.GetBytes(tmpPassword);
+
+
+            return new SHA384Managed().ComputeHash(passBytes);
+        }
+        public bool CheckIfAdmin(string username, string password)
+        {
+            string U = "Admin";
+            string P = "Admin";
+            //bool isAdmin = true;
+            bool isAdmin = (U == username && P == password);
+
+            //byte[] hashedP = O.HashUser(U, P);
+            //byte[] hashedP2 = O.HashUser(username, password);
+
+            //for (int i = 0; i < hashedP.Length; i++)
+            //{
+            //    if (hashedP[i] != hashedP2[i])
+            //    {
+            //        isAdmin = false;
+            //    }
+            //}
+
+            return isAdmin;
+        }
+    }
+}
---
+++ PantryPassionGUI/Views/RegisterPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PantryPassionGUI.Models;
+using PantryPassionGUI.Operations;
+
+namespace PantryPassionGUI.Views
+{
+    /// <summary>
+    /// Interaction logic for RegisterPage.xaml
+    /// </summary>
+    public partial class RegisterPage : Window
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+           // WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            Topmost = true;
+        }
+
+        /**
+         * Register method to handle the Register Button
+         * @param object sender
+         * @param RoutedEventArgs e
+         */
+        private void btnReg_Click(object sender, RoutedEventArgs e)
+        {
+            string username = tbxUsername.Text;
+            string password = pbxPassword.Password;
+            string fullname = tbxFullName.Text;
+
+
+            ApiOperations ops = new ApiOperations();
+            User user = ops.RegisterUser(username, password, fullname);
+            if (user == null)
+            {
+                MessageBox.Show("Username already exists");
+                return;
+            }
+
+            Globals.LoggedInUser = user;
+            MessageBox.Show("Registration successful");
+            // NavigationService.Navigate(new DetailsPage());
+            //NavigationService.GoBack(); // If page
+
+        }
+
+        /**
+         * Method to handle going back to the previous screen
+         * @param object  sender
+         * @param RoutedEventArgs e
+         */
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+         //   NavigationService.GoBack(); // if page
+           Close(); // If window
+        }
+    }
+}
+
