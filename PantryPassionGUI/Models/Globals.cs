f7f5020fdb41bbddbdd495c9d978c58e02f0f121
Laurits H.G <201905745@post.au.dk>
2021-05-07 11:07:01 +0200

    Added Logout

---
+++ PantryPassionGUI/Models/Globals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PantryPassionGUI.Models;
+
+namespace PantryPassionGUI.Models
+{
+    class Globals
+    {
+        public static User LoggedInUser { get; set; }
+        
+    }
+}
