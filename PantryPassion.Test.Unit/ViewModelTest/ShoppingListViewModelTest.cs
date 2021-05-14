2264b78c3dbf3eaf83459f9534aef68c7eaf06d3
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2021-05-14 11:32:01 +0200

    Added test for shoppinglist

---
+++ PantryPassion.Test.Unit/ViewModelTest/ShoppingListViewModelTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PantryPassionGUI.Models;
+using PantryPassionGUI.ViewModels;
+
+namespace PantryPassion.Test.Unit.ViewModelTest
+{
+    class ShoppingListViewModelTest
+    {
+        private ShoppingListViewModel _uut;
+        private object _obj;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new ShoppingListViewModel();
+            _obj = new object();
+        }
+
+        [Test]
+        public void ShoppingListViewModel_ClearListCommand_CalledBackendServer()
+        {
+            _uut.ClearListCommand.Execute(_obj);
+            //See if DeleteShoppingList() is called
+        }
+
+        [Test]
+        public void ShoppingListViewModel_UpdateSelectedItemCommand_CalledSetQuantity()
+        {
+            _uut.UpdateSelectedItemCommand.Execute(_obj);
+            //see if SetQuantity() is called
+        }
+
+        [Test]
+        public void ShoppingListViewModel_ClearListCommand_CalledDeleteShoppingList()
+        {
+            _uut.ClearListCommand.Execute(_obj);
+            //see if DeleteShoppingList() is called
+        }
+
+        [Test]
+        public void ShoppingListViewMode_AddItemToListCommand_FindItemViewOpen()
+        {
+            //make findItem view public get and private get
+        }
+    }
+}
