d9ee51e77037d5b12076476a4810b463d93d193a
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2021-05-15 11:35:34 +0200

    fix soundPlayer

--- PantryPassionGUI/Models/SoundPlayer.cs
+++ PantryPassionGUI/Models/SoundPlayer.cs
@@ -8,8 +8,11 @@ namespace PantryPassionGUI.Models
 {
     public class SoundPlayer : ISoundPlayer
     {
+        public System.Media.SoundPlayer _soundPlayer { get; private set; }
+        string _path = Environment.CurrentDirectory + @"\barcodeSoundBeep.wav";
         public SoundPlayer()
         {
+            _soundPlayer = new System.Media.SoundPlayer(_path);
             Mute = false;
         }
 
@@ -19,15 +22,7 @@ namespace PantryPassionGUI.Models
         {
             if (Mute == false)
             {
-                //Console.Beep();
-
-                //System.Media.SystemSounds.Beep.Play();
-
-                string path = Environment.CurrentDirectory + @"\barcodeSoundBeep.wav";
-
-                System.Media.SoundPlayer soundPlayer =
-                    new System.Media.SoundPlayer(path);
-                soundPlayer.Play();
+                _soundPlayer.Play();
             }
         }
     }
