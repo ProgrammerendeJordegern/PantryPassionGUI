b71ef4c3a97c8f249aeacc54ee7babf776da34f9
Laurits H.G <201905745@post.au.dk>
2021-05-15 11:58:30 +0200

    Added EditItem Functionality

--- PantryPassionGUI/ViewModels/EditItemsViewModel.cs
+++ PantryPassionGUI/ViewModels/EditItemsViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 using PantryPassionGUI.Models;
+using PantryPassionGUI.Utilities;
 using PantryPassionGUI.Views;
 using Prism.Commands;
 using Prism.Mvvm;
@@ -14,20 +17,74 @@ namespace PantryPassionGUI.ViewModels
 {
     public class EditItemsViewModel : BindableBase
     {
-        private Item theitem;
+        private ICommand _findItemCommand;
+        private BackendConnection _backendConnection = new BackendConnection();
 
-        public Item theItem
+        //Ok button
+        public ICommand FindItemCommand
+        {
+            get
+            {
+                return _findItemCommand ??= new DelegateCommand(FindItemHandler);
+            }
+        }
+        private void FindItemHandler()
+        {
+            FindItemView FIW = new FindItemView();
+            if (FIW.ShowDialog() == true)
+            {
+                MessageBox.Show("FIW IS TRUE");
+            }
+            TheItem = Globals.FoundItem;
+
+        }
+
+        private ICommand _saveItemCommand;
+
+        //Ok button
+        public ICommand SaveItemCommand
+        {
+            get
+            {
+                return _saveItemCommand ??= new DelegateCommand(SaveItemHandler);
+            }
+        }
+        private async void SaveItemHandler()
+        {
+            try
+            {
+                Item ItemToSend = TheItem.Item;
+                InventoryItem inventoryItemToSend = TheItem;
+                await _backendConnection.EditItem(ItemToSend);
+                await _backendConnection.SetQuantity(inventoryItemToSend);
+            }
+            catch (ApiException e)
+            {
+                MessageBox.Show($"Fejl {e.StatusCode}", "Error!");
+            }
+            catch (HttpRequestException exception)
+            {
+                MessageBox.Show($"Der er ingen forbindele til serveren", "Error!");
+            }
+            finally
+            {
+                MessageBox.Show("Item Updated Successfully");
+            }
+
+            Globals.FoundItem = null;
+        }
+
+
+        private InventoryItem theitem;
+
+        public InventoryItem TheItem
         {
             get { return theitem; }
             set
             {
                 SetProperty(ref theitem, value);
+                RaisePropertyChanged("TheItem");
             }
-        }
-
-        public EditItemsViewModel()
-        {
-            theItem = new Item("");
         }
 
         //ICommand _findItemCommand;
